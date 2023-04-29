7e9424108af5f4761ce4766e561c5a9f45e70981
tedy97123 <tedyyohanes97@gmail.com>
2023-04-29 00:01:55 -0700

    basics of enemy movment, needs work...

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+        transform.position = transform.position + horizontal * Time.deltaTime;
+    }
+}
